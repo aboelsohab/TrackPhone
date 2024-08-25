5df75183c7f24faa1e2eaeedeaf04b0997baaff3
aboelsohab <149389773+aboelsohab@users.noreply.github.com>
2024-08-25 03:49:48 +0300

    Update AdminReceiver.java

--- app/src/main/java/com/beta/trackphone/AdminReceiver.java
+++ app/src/main/java/com/beta/trackphone/AdminReceiver.java
@@ -2,7 +2,18 @@ package com.beta.trackphone;
 
 import android.app.admin.DeviceAdminReceiver;
 
+// سيتم استخدام هذا لإرسال إشعارات عندما يتم التحقق من أن الهاتف قد سرق أو عند تفعيل Lost Phone Mode
+
 public class AdminReceiver extends DeviceAdminReceiver {
 
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        Toast.makeText(context, "Device Admin: enabled", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+        Toast.makeText(context, "Device Admin: disabled", Toast.LENGTH_SHORT).show();
+    }
 
 }
